ce48aedf0fff38e6bfd9bc9d4e062ffb419d63a2
Georgi Kiryakov <george.kiryakov@gmail.com>
2018-06-15 17:11:14 +0300

    fix edge case bug

--- code/14-07-2015-1-img.cpp
+++ code/14-07-2015-1-img.cpp
@@ -3,15 +3,28 @@ using namespace std;
 #define MAX_M 10
 #define MAX_N 10
 
+inline bool is_inside(int x, int y, int M, int N) {
+  return x >= 0 && x < M && y >= 0 && y < N;
+}
+
+float avg_pixel(int x, int y, float img[MAX_M][MAX_N], int M, int N) {
+  float sum = 0;
+  int size = 0;
+  for (int i = x*2; i <= x*2 + 1; i++)
+    for (int j=y*2; j <= y*2 + 1; j++)
+      if (is_inside(i, j, M, N)) {
+        sum += img[i][j];
+        size += 1;
+      }
+  return sum / size;
+}
+
 void subsample(float img[MAX_M][MAX_N], int m, int n) {
   int s_m = (m + 1) / 2;
   int s_n = (n + 1) / 2;
   for(int i=0; i < s_m; i++) {
     for(int j=0; j < s_n; j++) {
-      float avg = (img[i*2][j*2] + img[i*2][j*2 + 1] +:
-          img[i*2 + 1][j*2] + img[i*2 + 1][j*2 + 1]) / 4.0;
-
-      cout << avg << " ";
+      cout << avg_pixel(i, j, img, m, n) << " ";
     }
     cout << endl;
   }
